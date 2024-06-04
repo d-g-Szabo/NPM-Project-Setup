f7fbab97bb2234b3fa8711afb6ac937b3056e167
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-06-04 14:52:30 +0100

    done

--- script.js
+++ script.js
@@ -6,3 +6,13 @@
 // for ES6 to work, I need to specify the type of app that we are running in the package.json file
 import cowsay from "cowsay"; //ES6
 console.log(cowsay.say({ text: "I am a mooooooooooodule!", e: "oO", t: "U" }));
+
+import chalk from "chalk";
+import { log } from "console";
+console.log(chalk.redBright("Hello world!"));
+
+log(`
+CPU: ${chalk.red("90%")}
+RAM: ${chalk.green("40%")}
+DISK: ${chalk.yellow("70%")}
+`);
